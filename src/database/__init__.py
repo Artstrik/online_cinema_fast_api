ee0f6079659e817f452e1e29497d593ca1c12786
Artem Shlychkin <artemshlychkin1995@gmail.com>
2026-02-16 21:11:33 +0200

    feat(database: setup PostgreSQL with SQLAlchemy async)

---
+++ src/database/__init__.py
@@ -0,0 +1,18 @@
+import os
+
+from src.database.models.base import Base
+
+from src.database.session_sqlite import reset_sqlite_database as reset_database
+
+environment = os.getenv("ENVIRONMENT", "developing")
+
+if environment == "testing":
+    from src.database.session_sqlite import (
+        get_sqlite_db_contextmanager as get_db_contextmanager,
+        get_sqlite_db as get_db
+    )
+else:
+    from src.database.session_postgresql import (
+        get_postgresql_db_contextmanager as get_db_contextmanager,
+        get_postgresql_db as get_db
+    )
