d0e6224676bf8e650520cbadb9eda00bb85a1c7e
Artem Shlychkin <artemshlychkin1995@gmail.com>
2026-02-19 15:54:48 +0200

    feat(infra): setup Celery with redis, and update docker files

--- src/database/models/__init__.py
+++ src/database/models/__init__.py
@@ -1,2 +1,57 @@
-class MovieModel:
-    pass
+"""
+Database models for the Theater application.
+
+This module exports all database models for easy import.
+"""
+
+from src.database.models.accounts import (
+    UserModel,
+    UserGroupModel,
+    UserProfileModel,
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel,
+    UserGroupEnum,
+    GenderEnum,
+)
+
+from src.database.models.movies import (
+    MovieModel,
+    GenreModel,
+    ActorModel,
+    DirectorModel,
+    CertificationModel,
+    CountryModel,
+    LanguageModel,
+    MovieLikeModel,
+    MovieCommentModel,
+    MovieFavoriteModel,
+    MovieRatingModel,
+    MovieStatusEnum,
+)
+
+__all__ = [
+    # Account models
+    "UserModel",
+    "UserGroupModel",
+    "UserProfileModel",
+    "ActivationTokenModel",
+    "PasswordResetTokenModel",
+    "RefreshTokenModel",
+    "UserGroupEnum",
+    "GenderEnum",
+    # Movie models
+    "MovieModel",
+    "GenreModel",
+    "ActorModel",
+    "DirectorModel",
+    "CertificationModel",
+    "CountryModel",
+    "LanguageModel",
+    "MovieLikeModel",
+    "MovieCommentModel",
+    "MovieFavoriteModel",
+    "MovieRatingModel",
+    "MovieStatusEnum",
+
+]
